a45500e0b8e8825fecc8305b8c1e8335cccc7c12
Liam Vuong <lamvd0101@gmail.com>
2022-11-02 13:30:11 +0700

    Sync

---
+++ Frameworks/RNSDK.xcframework/ios-arm64_x86_64-simulator/RNSDK.framework/Headers/RNSDK.h
@@ -0,0 +1,19 @@
+//
+//  RNSDK.h
+//  RNSDK
+//
+//  Created by LAP02092 on 02/11/2022.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RNSDK.
+FOUNDATION_EXPORT double RNSDKVersionNumber;
+
+//! Project version string for RNSDK.
+FOUNDATION_EXPORT const unsigned char RNSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNSDK/PublicHeader.h>
+
+
